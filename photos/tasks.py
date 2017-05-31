7b10360d7e380671f4146055c098136b399c79d4
Anubhav722 <anubhavs286@gmail.com>
2017-05-31 12:01:34 +0530

    add templates

---
+++ photos/tasks.py
@@ -0,0 +1,15 @@
+from celery.task.schedules import crontab
+from celery.decorators import periodic_task
+from celery.utils.logs import get_task_logger
+
+from photos.utils import save_latest_flickr_image
+
+logger = get_task_logger(__name__)
+
+@periodic_task(run_every=(crontab(minute='*/15')), name='task_save_latest_flickr_image', ignore_result=True)
+def task_save_latest_flickr_image():
+	"""
+	Saves latest flickr image
+	"""
+	save_latest_flickr_image()
+	logger.info("Saved image from Flickr")
